32d925c27e25edf71f06015c0350c674400d1e60
romainprignon <pro.rprignon@gmail.com>
2026-02-12 20:26:29 +0100

    ✨ feat: add dockerfiles

--- workspaces/worker/src/app/config.ts
+++ workspaces/worker/src/app/config.ts
@@ -2,4 +2,5 @@ import { resolve } from 'node:path'
 import { Config } from '@iot/libs'
 import { type ConfigMap } from '../config/config.schema.js'
 
-export default new Config<ConfigMap>(resolve('./src/config'))
+// eslint-disable-next-line n/no-unsupported-features/node-builtins
+export default new Config<ConfigMap>(resolve(import.meta.dirname + '/../config'))
